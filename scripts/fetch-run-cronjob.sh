6793335ebe667c92ba16e522f4e524509f9f39aa
Jon Sowman <jon@hexoc.com>
2012-03-10 11:22:07 +0000

    Some error handling in the fetch and grab cronjob

--- scripts/fetch-run-cronjob.sh
+++ scripts/fetch-run-cronjob.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-# Script to run the two cronjobs (fetch and grab) consecutively
+# Script to run the two cronjobs (fetch and grab) consecutively.
+# Ensure this script is executable by the user whose crontab will
+# be used to run it.
 #
 # Jon Sowman 2012 <jon@hexoc.com>
 #
@@ -12,6 +14,17 @@ FETCHER=${ROOT}/cusf-landing-prediction/scripts/grabdata-cronjob.sh
 
 # The predictor cronjob to run predictions for the next x days
 HOURLY=${ROOT}/cusf-landing-prediction/scripts/hourly-predictions-cronjob.sh
+
+# Check that the scripts exist and are executable
+if [ ! -x ${FETCHER} ]; then
+    echo "$0: ${FETCHER} does not exist or is not executable."
+    exit 1
+fi
+
+if [ ! -x ${HOURLY} ]; then
+    echo "$0: ${HOURLY} does not exist or is not executable."
+    exit 1
+fi
 
 # Call each of the scripts in turn
 ${FETCHER}

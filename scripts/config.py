4eca6a62cbd7b9ebcd1a9216a955a5e939710010
Jon Sowman <jon@hexoc.com>
2012-03-10 01:45:07 +0000

    Fixed paths in config python file

--- scripts/config.py
+++ scripts/config.py
@@ -4,9 +4,9 @@ import os
 
 CUSF_HOME = '/opt/'
 CUSF_PYTHON_PATH = '/opt/python-packages/'
-HOURLY_PREDICTIONS = os.path.join(CUSF_HOME, '/cusf-landing-prediction/web/hourly-predictions')
-LAND_PRED_APP = os.path.join(CUSF_HOME, '/cusf-landing-prediction/pred_src/pred')
-LAND_PRED_DATA = os.path.join(CUSF_HOME, '/landing-prediction-data/gfs/')
+HOURLY_PREDICTIONS = os.path.join(CUSF_HOME, 'cusf-landing-prediction/web/hourly-predictions')
+LAND_PRED_APP = os.path.join(CUSF_HOME, 'cusf-landing-prediction/pred_src/pred')
+LAND_PRED_DATA = os.path.join(CUSF_HOME, 'landing-prediction-data/gfs/')
 
 # vim:sw=4:ts=4:et:autoindent
 

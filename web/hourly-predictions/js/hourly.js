5e6e9a9acfb1474cc79303a0f545f09027088ec1
Jon Sowman <jon@hexoc.com>
2012-05-31 08:34:51 +0100

    Okay, issue #8 fixes it, the actual commit fixing the bug was aafeff066

--- web/hourly-predictions/js/hourly.js
+++ web/hourly-predictions/js/hourly.js
@@ -148,9 +148,7 @@ function populate_map() {
                 $.each(predictions, function(idx, prediction) {
                         // console.log(prediction);
                         var where = prediction.entry['landing-location'];
-                        console.log("Pre converted is " + prediction.entry['launch-time'].month);
                         var launch_time = prediction_entry_convert_date(prediction.entry['launch-time']);
-                        console.log("Post converted is " + launch_time)
                         var landing_time = prediction_entry_convert_date(prediction.entry['landing-time']);
 
                         // Derive the index for the greyscale marker image
